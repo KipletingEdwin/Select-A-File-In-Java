ab1da643535717743bcbeb68de08d67cf2c19ffa
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-17 17:53:08 +0300

    add MyFrame class

---
+++ src/MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.jar.JarFile;
+
+public class MyFrame extends JFrame implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
