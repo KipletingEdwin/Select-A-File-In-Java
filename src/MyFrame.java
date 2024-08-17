b121607fe9699bfbaf355e5003b6ea818132991f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-17 17:56:14 +0300

    add pack

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -9,6 +9,7 @@ public class MyFrame extends JFrame implements ActionListener {
     MyFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
+        this.pack();
 
     }
     @Override
