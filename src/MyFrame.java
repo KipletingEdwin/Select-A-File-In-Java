1ddd27b4bf6a1af21a0c3efbb2cc54011f88b007
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-17 17:56:47 +0300

    setVisibility to true

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -10,6 +10,7 @@ public class MyFrame extends JFrame implements ActionListener {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
         this.pack();
+        this.setVisible(true);
 
     }
     @Override
